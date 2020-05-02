c5006f3081abedeca1e624b9add1a1cea1005432
antoinechalifour <antoine.chalifour@gmail.com>
2020-05-02 17:06:32 +0200

    test: add LeaveSpace tests

---
+++ packages/server/src/usecase/LeaveSpace.test.ts
@@ -0,0 +1,50 @@
+import { User } from "../domain/entities/User";
+import { Space } from "../domain/entities/Space";
+import { SpaceMemoryAdapter } from "../infrastructure/SpaceMemoryAdapter";
+import { NotificationPort } from "./ports/NotificationPort";
+import { SpacePort } from "./ports/SpacePort";
+import { LeaveSpace } from "./LeaveSpace";
+
+const mockNotificationPort = () => ({
+  notifyRtcIceCandidateReceived: jest.fn(),
+  notifyRtcAnswerReceived: jest.fn(),
+  notifyRtcOfferReceived: jest.fn(),
+  notifySpaceJoined: jest.fn(),
+  notifySpaceLeft: jest.fn(),
+});
+
+describe("LeaveSpace", () => {
+  let space: Space;
+  let notificationPort: NotificationPort;
+  let spacePort: SpacePort;
+  let leaveSpace: LeaveSpace;
+  let currentUser: User;
+
+  beforeEach(() => {
+    spacePort = new SpaceMemoryAdapter();
+    notificationPort = mockNotificationPort();
+    space = Space.createSpace();
+    currentUser = User.create(space.id, "user-name");
+
+    leaveSpace = new LeaveSpace({
+      spacePort,
+      notificationPort,
+      currentUser,
+    });
+  });
+
+  it("should notify other users in the space", async () => {
+    // Given
+    await spacePort.saveSpace(space);
+
+    // When
+    await leaveSpace.execute();
+
+    // Then
+    expect(notificationPort.notifySpaceLeft).toHaveBeenCalledTimes(1);
+    expect(notificationPort.notifySpaceLeft).toHaveBeenCalledWith(
+      space,
+      currentUser
+    );
+  });
+});
