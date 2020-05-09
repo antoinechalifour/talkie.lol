6e82ec69db0d28d5021d18d540e54a2f40a70fce
antoinechalifour <antoine.chalifour@gmail.com>
2020-05-09 14:52:59 +0200

    chore: remove unused test util

--- packages/web/src/test-utils/remoteUser.ts
+++
@@ -1,8 +0,0 @@
-import { RemoteUser } from "../models/RemoteUser";
-
-export const mockRemoteUser = (id: string, name: string): RemoteUser =>
-  new (class extends RemoteUser {
-    constructor() {
-      super(id, name);
-    }
-  })();
